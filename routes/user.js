128a62c76cc164c893fd28c1336a35a8e92d3b45
Malik Muneeb <malikmuneeb981@gmail.com>
2022-10-13 21:24:16 +0500

    more

--- routes/user.js
+++ routes/user.js
@@ -90,7 +90,7 @@ router.post("/register", async (req, res, next) => {
       email: email,
     });
     if (isuser) {
-      return res.json({
+      return res.status(400).json({
         msg: "User already exists",
         success: false,
         user: null,
@@ -106,7 +106,7 @@ router.post("/register", async (req, res, next) => {
       let size = 200;
       user.avatar = "https://gravatar.com/avatar/?s=" + size + "&d=retro";
       await user.save();
-      return res.json({
+      return res.status(200).json({
         msg: "Register Success",
         success: true,
         user: user,
